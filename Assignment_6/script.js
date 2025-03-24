585392a9319d1076900204ddf40fe0aeb0baa570
Karli Langer <klanger1@pride.hofstra.edu>
2025-03-24 12:53:37 -0400

    commit

---
+++ Assignment_6/script.js
@@ -0,0 +1,12 @@
+// 1. Display a message in the browser console
+console.log("CSS + JavaScript is powerful!");
+
+// 2. Show an alert message when the page loads
+window.onload = function () {
+    alert("Welcome to Assignment 6 with JavaScript!");
+};
+
+// 3. Function to toggle between light mode and dark mode
+function changeTheme() {
+    document.body.classList.toggle("dark-mode");
+}
